87a365f83d470f780b6b637eb6743d14dd6a06fb
Kai <k.davey4@icloud.com>
2026-06-30 22:40:46 -0700

    feat: example site design from paper

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
+/**
+ * from Paper
+ * https://app.paper.design/file/01KW9579WBJWDP4WSAC4Z6NMWP/1-0/159-0
+ * on Jun 30, 2026
+ */
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <div style={{ alignItems: 'center', backgroundColor: '#FFFFFF', boxSizing: 'border-box', display: 'flex', flexDirection: 'column', fontSize: '12px', fontSynthesis: 'none', height: '3047px', lineHeight: '16px', MozOsxFontSmoothing: 'grayscale', WebkitFontSmoothing: 'antialiased', width: '1500px' }}>
+      <div style={{ alignItems: 'center', alignSelf: 'stretch', boxShadow: '#0000001A 0px 2px 20px -2px', boxSizing: 'border-box', display: 'flex', gap: 281, justifyContent: 'space-between', paddingBlock: '16px', paddingInline: '48px' }}>
+        <div style={{ alignItems: 'center', boxSizing: 'border-box', display: 'flex', gap: '36px', justifyContent: 'center' }}>
+          <div style={{ backgroundImage: 'url(https://app.paper.design/file-assets/01KW9579WBJWDP4WSAC4Z6NMWP/15Q27M4JECTEB0VG1QH98V9EXY.webp)', backgroundPosition: '50%', backgroundRepeat: 'no-repeat', backgroundSize: 'round(100%, 0.5px) round(100%, 0.5px)', boxSizing: 'border-box', flexShrink: '0', height: '75px', overflow: 'clip', width: '75px' }} />
+          <div style={{ boxSizing: 'border-box', color: '#812D7F', display: 'flex', flexShrink: '0', fontFamily: 'system-ui, sans-serif', fontSize: '24px', fontWeight: 600, letterSpacing: '-0.3px', lineHeight: '24.92px', width: 'max-content' }}>
+            Home
+          </div>
+          <div style={{ boxSizing: 'border-box', color: '#812D7F', display: 'flex', flexShrink: '0', fontFamily: 'system-ui, sans-serif', fontSize: '24px', fontWeight: 600, letterSpacing: '-0.3px', lineHeight: '24.92px', width: 'max-content' }}>
+            About
+          </div>
+          <div style={{ boxSizing: 'border-box', color: '#812D7F', display: 'flex', flexShrink: '0', fontFamily: 'system-ui, sans-serif', fontSize: '24px', fontWeight: 600, letterSpacing: '-0.3px', lineHeight: '24.92px', width: 'max-content' }}>
+            Careers
+          </div>
+          <div style={{ boxSizing: 'border-box', color: '#812D7F', display: 'flex', flexShrink: '0', fontFamily: 'system-ui, sans-serif', fontSize: '24px', fontWeight: 600, height: '25px', letterSpacing: '-0.3px', lineHeight: '24.92px', width: 'max-content' }}>
+            For Students
+          </div>
+          <div style={{ boxSizing: 'border-box', color: '#812D7F', display: 'flex', flexShrink: '0', fontFamily: 'system-ui, sans-serif', fontSize: '24px', fontWeight: 600, height: '25px', letterSpacing: '-0.3px', lineHeight: '24.92px', width: 'max-content' }}>
+            For Companies
+          </div>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
+        <div style={{ alignItems: 'start', backgroundColor: '#7F2D7F', borderColor: '#FFFFFF', borderRadius: '300px', borderStyle: 'solid', borderWidth: '1px', boxSizing: 'border-box', display: 'flex', flexDirection: 'column', gap: 9, paddingBlock: '10px', paddingInline: '28px' }}>
+          <div style={{ boxSizing: 'border-box', color: '#FFFFFF', display: 'inline-block', fontFamily: 'system-ui, sans-serif', fontSize: '18px', fontWeight: 700, lineHeight: '22px', textAlign: 'center', width: 'max-content' }}>
+            Join Us
+          </div>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+      </div>
+      <div style={{ alignItems: 'start', alignSelf: 'stretch', boxSizing: 'border-box', display: 'flex', gap: '50px', justifyContent: 'center', paddingBlock: '100px' }}>
+        <div style={{ backgroundImage: 'url(https://app.paper.design/file-assets/01KW9579WBJWDP4WSAC4Z6NMWP/01KWE1JR70T79W2R34Q1474M25.png)', backgroundPosition: '50%', backgroundSize: 'cover', borderRadius: '20px', boxSizing: 'border-box', flexShrink: '0', height: '490px', width: '649px' }} />
+        <div style={{ alignItems: 'start', boxSizing: 'border-box', display: 'flex', flexDirection: 'column', gap: '32px' }}>
+          <div style={{ alignItems: 'start', boxSizing: 'border-box', display: 'flex', flexDirection: 'column', gap: '8px', width: '595px' }}>
+            <div style={{ alignItems: 'center', boxSizing: 'border-box', display: 'flex', gap: '14px' }}>
+              <div style={{ boxSizing: 'border-box', color: '#000000', flexShrink: '0', fontFamily: 'system-ui, sans-serif', fontSize: '60px', fontWeight: 500, lineHeight: '65.675px', width: 'fit-content' }}>
+                We bridge
+              </div>
+              <div style={{ boxSizing: 'border-box', color: '#802E7F', fontFamily: 'system-ui, sans-serif', fontSize: '60px', fontWeight: 500, lineHeight: '65.675px' }}>
+                product
+              </div>
+            </div>
+            <div style={{ alignItems: 'start', boxSizing: 'border-box', display: 'flex', gap: '14px' }}>
+              <div style={{ boxSizing: 'border-box', color: '#802E7F', fontFamily: 'system-ui, sans-serif', fontSize: '60px', fontWeight: 500, lineHeight: '65.675px' }}>
+                strategy
+              </div>
+              <div style={{ boxSizing: 'border-box', color: '#000000', flexShrink: '0', fontFamily: 'system-ui, sans-serif', fontSize: '60px', fontWeight: 500, lineHeight: '65.675px', width: 'fit-content' }}>
+                and execution
+              </div>
+            </div>
+            <div style={{ boxSizing: 'border-box', color: '#000000', fontFamily: 'system-ui, sans-serif', fontSize: '60px', fontWeight: 500, lineHeight: '65.675px', width: 'fit-content' }}>
+              to help your team
+            </div>
+            <div style={{ boxSizing: 'border-box', color: '#000000', fontFamily: 'system-ui, sans-serif', fontSize: '60px', fontWeight: 500, lineHeight: '65.675px', width: 'fit-content' }}>
+              move faster.
+            </div>
+          </div>
+          <div style={{ boxSizing: 'border-box', color: '#000000', fontFamily: 'system-ui, sans-serif', fontSize: '20px', fontWeight: 500, lineHeight: '30px', width: '585px' }}>
+            We connect tech companies with top product talent from UCLA to work on high-priority business goals across product management, design and marketing via quarter-long projects.
+          </div>
+          <div style={{ alignItems: 'center', backgroundColor: '#B265B0', borderColor: '#FFFFFF', borderRadius: '20px', borderStyle: 'solid', borderWidth: '1px', boxSizing: 'border-box', display: 'flex', flexDirection: 'column', gap: 9, justifyContent: 'center', paddingBlock: '20px', paddingInline: '70px' }}>
+            <div style={{ boxSizing: 'border-box', color: '#FFFFFF', display: 'inline-block', fontFamily: 'system-ui, sans-serif', fontSize: '22px', fontWeight: 700, lineHeight: '28px', textAlign: 'center', width: 'max-content' }}>
+              Contact Us
+            </div>
+          </div>
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+      </div>
+    </div>
+  );
 }
 
 export default App

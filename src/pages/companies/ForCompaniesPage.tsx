e0ee42dc43cbebb505d47b0264969cfc08d28993
Kai <k.davey4@icloud.com>
2026-09-10 00:11:09 -0700

    fix: adjusted width of for companies page

--- src/pages/companies/ForCompaniesPage.tsx
+++ src/pages/companies/ForCompaniesPage.tsx
@@ -1,4 +1,4 @@
-import styles from './ForCompaniesPage.module.css'
+import { PageContent } from '@/components/PageContent'
 import { companies } from '@/content/companies'
 import { usePageTitle } from '@/lib/usePageTitle'
 import { Contact } from './Contact'
@@ -11,13 +11,13 @@ import { Timeline } from './Timeline'
 export function ForCompaniesPage() {
   usePageTitle(companies.title)
   return (
-    <div className={styles.page}>
+    <PageContent offsetHeader>
       <Overview />
       <Timeline />
       <Testimonials />
       <Partnerships />
       <PastEvents />
       <Contact />
-    </div>
+    </PageContent>
   )
 }
